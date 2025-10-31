a84afd3900fe2f8d6ab9c963bfb6da3b16111541
ldtrees <dstring2@proton.me>
2025-10-31 17:16:14 +0200

    chore: add multipart, cors, new routes

--- server.js
+++ server.js
@@ -3,24 +3,27 @@ import "dotenv/config";
 import Fastify from "fastify";
 import { db_initialization } from "./src/database/initialization.js";
 import { pool } from "./src/database/connection.js";
-import { processIngredientsFromImage } from "./src/controllers/new_toothpaste_analyzer.js";
+import toothpasteRoutes from "./src/routes/toothpaste.routes.js";
+import fastifyMultipart from "@fastify/multipart";
+import cors from "@fastify/cors";
 
 const fastify = Fastify({
 	logger: true,
 });
 
-fastify.get("/", async (request, reply) => {
-	return { hi: "world" };
+await fastify.register(cors, {
+	origin: "*",
+	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	allowedHeaders: ["Content-Type", "Authorization"],
 });
 
-fastify.get("/test", async (request, reply) => {
-	const { t } = await processIngredientsFromImage({ imageUrl: "" });
-	return { hi: t };
-});
+fastify.register(fastifyMultipart, { attachFieldsToBody: true });
+fastify.register(toothpasteRoutes, { prefix: "/api/v1" });
 
 const start = async () => {
 	try {
 		await db_initialization();
+		console.log("\n✅ Database setup complete!");
 		await fastify.listen({ port: 3000 });
 		console.log("Server running at http://localhost:3000");
 	} catch (err) {
