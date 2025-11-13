40151a61430e40ccd4f4dc31a4018ce1da3350bd
ldtrees <dstring2@proton.me>
2025-11-13 21:44:16 +0200

    chore: minor adjustments

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,11 +3,11 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-	host: process.env.PG_HOST || "localhost",
-	user: process.env.PG_USER || "postgres",
-	password: process.env.PG_PASSWORD || "",
-	database: process.env.PG_DATABASE || "devdb",
-	port: process.env.PG_PORT || 5433,
+	host: process.env.DB_HOST || "localhost",
+	user: process.env.DB_USER || "postgres",
+	password: process.env.DB_PASSWORD || "",
+	database: process.env.DB_NAME || "devdb",
+	port: process.env.DB_PORT || 5433,
 	max: 50,
 	idleTimeoutMillis: 15000,
 	connectionTimeoutMillis: 2000, // Return an error after 2s if connection could not be established
