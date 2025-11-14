853f25f87eb08e5442e860711a9298ed3c2a3c59
ldtrees <dstring2@proton.me>
2025-11-14 13:03:10 +0200

    chore: Change server port from 3000 to 3001

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ fastify.register(toothpasteRoutes, { prefix: "/api/v1" });
 
 const start = async () => {
 	try {
-		await fastify.listen({ port: 3000 });
-		console.log("Server running at http://localhost:3000");
+		await fastify.listen({ port: 3001 });
+		console.log("Server running at http://localhost:3001");
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
